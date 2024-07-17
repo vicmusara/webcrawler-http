b61b0afddebd74cc5e2268859741b47856467d17
vmg <musaravgd@gmail.com>
2024-07-17 12:24:57 +0200

    normalise urls

---
+++ crawl.test.js
@@ -0,0 +1,24 @@
+const {normalizeURL} = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
+
+
+test('normalizeURl strip protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURl strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURl capitals', () => {
+    const input = 'https://BLOG.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
